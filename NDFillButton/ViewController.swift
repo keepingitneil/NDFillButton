f0ab120d704de6b53ef30cf7d45540fc7abc470d
Neil Dwyer <neildwyer@Neils-MBP.hq.monkeyinferno.com>
2015-08-05 17:12:38 -0700

    text the fix.

--- NDFillButton/ViewController.swift
+++ NDFillButton/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        button.activeText = "Following"
-        button.normalText = "Follow"
+        button.setActive(true, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
